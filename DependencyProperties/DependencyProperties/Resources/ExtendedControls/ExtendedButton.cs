a7f7766fb0444cd0a77e3b512329d1855703db44
Nerada <5610242+Nerada@users.noreply.github.com>
2020-12-09 11:10:07 +0100

    Added ExtendedButton

---
+++ DependencyProperties/DependencyProperties/Resources/ExtendedControls/ExtendedButton.cs
@@ -0,0 +1,171 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DependencyProperties.Resources.ExtendedControls
+{
+    /// <summary>
+    /// Based on: https://stackoverflow.com/questions/815797/add-dependency-property-to-control
+    /// </summary>
+    public sealed class ExtendedButton : Button
+    {
+        /// <summary>
+        /// Foreground color extension
+        /// </summary>
+        public new static readonly DependencyProperty ForegroundProperty =
+            DependencyProperty.Register("Foreground", typeof(Brush), typeof(ExtendedButton), new PropertyMetadata(default(Brush), ForegroundColorChangedCallback));
+
+        public static readonly DependencyProperty HasForegroundColorProperty =
+            DependencyProperty.Register("HasForegroundColor", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public bool HasForegroundColor => (bool)GetValue(HasForegroundColorProperty);
+
+        public new Brush Foreground
+        {
+            get => (Brush)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        private static void ForegroundColorChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasForegroundColorProperty, true);
+        }
+
+        /// <summary>
+        /// Background color extension
+        /// </summary>
+        public new static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(ExtendedButton), new PropertyMetadata(default(Brush), BackgroundColorChangedCallback));
+
+        public static readonly DependencyProperty HasBackgroundColorProperty =
+            DependencyProperty.Register("HasBackgroundColor", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public bool HasBackgroundColor => (bool)GetValue(HasBackgroundColorProperty);
+
+        public new Brush Background
+        {
+            get => (Brush)GetValue(BackgroundProperty);
+            set => SetValue(BackgroundProperty, value);
+        }
+
+        private static void BackgroundColorChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasBackgroundColorProperty, true);
+        }
+
+        /// <summary>
+        /// Background hover color extension
+        /// </summary>
+        public static readonly DependencyProperty HasBackgroundHoverColorProperty =
+            DependencyProperty.Register("HasBackgroundHoverColor", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty BackgroundHoverColorProperty =
+            DependencyProperty.Register("BackgroundHoverColor", typeof(Brush), typeof(ExtendedButton), new PropertyMetadata(default(Brush), BackgroundHoverColorChangedCallback));
+
+        public bool HasBackgroundHoverColor => (bool)GetValue(HasBackgroundHoverColorProperty);
+
+        public Brush BackgroundHoverColor
+        {
+            get => (Brush)GetValue(BackgroundHoverColorProperty);
+            set => SetValue(BackgroundHoverColorProperty, value);
+        }
+
+        private static void BackgroundHoverColorChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasBackgroundHoverColorProperty, true);
+        }
+
+        /// <summary>
+        /// Foreground hover color extension
+        /// </summary>
+        public static readonly DependencyProperty HasForegroundHoverColorProperty =
+            DependencyProperty.Register("HasForegroundHoverColor", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ForegroundHoverColorProperty =
+            DependencyProperty.Register("ForegroundHoverColor", typeof(Brush), typeof(ExtendedButton), new PropertyMetadata(default(Brush), ForegroundHoverColorChangedCallback));
+
+        public bool HasForegroundHoverColor => (bool)GetValue(HasForegroundHoverColorProperty);
+
+        public Brush ForegroundHoverColor
+        {
+            get => (Brush)GetValue(ForegroundHoverColorProperty);
+            set => SetValue(ForegroundHoverColorProperty, value);
+        }
+
+        private static void ForegroundHoverColorChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasForegroundHoverColorProperty, true);
+        }
+
+        /// <summary>
+        /// Image extension
+        /// </summary>
+        public static readonly DependencyProperty HasImageProperty =
+            DependencyProperty.Register("HasImage", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(DrawingImage), typeof(ExtendedButton), new PropertyMetadata(default(DrawingImage), ImageChangedCallback));
+
+        public static readonly DependencyProperty ImageMarginProperty =
+            DependencyProperty.Register("ImageMargin", typeof(Thickness), typeof(ExtendedButton), new PropertyMetadata(default(Thickness), ImageChangedCallback));
+
+        public bool HasImage => (bool)GetValue(HasImageProperty);
+
+        public DrawingImage Image
+        {
+            get => (DrawingImage)GetValue(ImageProperty);
+            set => SetValue(ImageProperty, value);
+        }
+
+        private static void ImageChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasImageProperty, true);
+        }
+
+        public Thickness ImageMargin
+        {
+            get => (Thickness)GetValue(ImageMarginProperty);
+            set => SetValue(ImageMarginProperty, value);
+        }
+
+        /// <summary>
+        /// Image hover extension
+        /// </summary>
+        public static readonly DependencyProperty HasImageHoverProperty =
+            DependencyProperty.Register("HasImageHover", typeof(bool), typeof(ExtendedButton), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ImageHoverProperty =
+            DependencyProperty.Register("ImageHover", typeof(DrawingImage), typeof(ExtendedButton), new PropertyMetadata(default(DrawingImage), ImageHoverChangedCallback));
+
+        public bool HasImageHover => (bool)GetValue(HasImageHoverProperty);
+
+        public DrawingImage ImageHover
+        {
+            get => (DrawingImage)GetValue(ImageHoverProperty);
+            set => SetValue(ImageHoverProperty, value);
+        }
+
+        private static void ImageHoverChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            (dependencyObject as ExtendedButton)?.SetValue(HasImageHoverProperty, true);
+        }
+
+        /// <summary>
+        /// Additional content extension
+        /// </summary>
+        public static readonly DependencyProperty Content2Property =
+            DependencyProperty.Register("Content2", typeof(string), typeof(ExtendedButton), new PropertyMetadata(default(string), Content2ChangedCallBack));
+
+        public string Content2
+        {
+            set => SetValue(Content2Property, value);
+        }
+
+        private static void Content2ChangedCallBack(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            if (!(dependencyObject as ExtendedButton is {} extendedButton)) return;
+
+            extendedButton.Content += (string)args.NewValue;
+        }
+    }
+}
---
+++ DependencyProperties/DependencyProperties/Resources/ExtendedControls/SegmentedScrollBar.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Prism.Commands;
+
+namespace DependencyProperties.Resources.ExtendedControls
+{
+    public class SegmentedScrollBar : ScrollBar
+    {
+        private enum ButtonType
+        {
+            LeftButton,
+            RightButton,
+        }
+
+        private readonly SegmentedScrollBarSegmentDrawing _segmentDrawing;
+
+        private List<double> _segmentBoundaries = new List<double>();
+
+        public SegmentedScrollBar()
+        {
+            Scroll += (sender, args) => OnScroll();
+
+            _segmentDrawing = new SegmentedScrollBarSegmentDrawing(this);
+
+            PreviousSegmentCommand = new DelegateCommand(() => OnButtonClick(ButtonType.LeftButton),  CanExecutePreviousSegmentCommand);
+            NextSegmentCommand     = new DelegateCommand(() => OnButtonClick(ButtonType.RightButton), CanExecuteNextSegmentCommand);
+        }
+
+        public static readonly DependencyProperty SegmentBoundariesProperty =
+            DependencyProperty.Register("SegmentBoundaries", typeof(DelegateCommand), typeof(SegmentedScrollBar));
+
+        public static readonly DependencyProperty PreviousSegmentCommandProperty =
+            DependencyProperty.Register("PreviousSegmentCommand", typeof(DelegateCommand), typeof(SegmentedScrollBar));
+
+        public static readonly DependencyProperty NextSegmentCommandProperty =
+            DependencyProperty.Register("NextSegmentCommand", typeof(DelegateCommand), typeof(SegmentedScrollBar));
+
+        public List<double> SegmentBoundaries
+        {
+            get => _segmentBoundaries;
+            set
+            {
+                _segmentBoundaries = value;
+                CheckSegmentBoundaries();
+            }
+        }
+
+        public DelegateCommand PreviousSegmentCommand
+        {
+            get => (DelegateCommand)GetValue(PreviousSegmentCommandProperty);
+            set => SetValue(PreviousSegmentCommandProperty, value);
+        }
+
+        public DelegateCommand NextSegmentCommand
+        {
+            get => (DelegateCommand)GetValue(NextSegmentCommandProperty);
+            set => SetValue(NextSegmentCommandProperty, value);
+        }
+
+        private bool CanExecutePreviousSegmentCommand() => Value > 0;
+
+        private bool CanExecuteNextSegmentCommand() => SegmentBoundaries.Count != 0 && SegmentBoundaries[^1] > Value;
+
+        private void OnButtonClick(ButtonType buttonType)
+        {
+            // check if it comes from the right or left button, depending on that, switch to right or left segment.
+            // If we go to the left direction - set scrollbar value to the end of the previous segment.
+            // If the direction is to the right - to the beginning of the next segment.
+
+            // Get current segment
+            int segmentIndex = SegmentBoundaries.IndexOf(SegmentBoundaries.FirstOrDefault(b => b > Value));
+            switch (buttonType)
+            {
+                case ButtonType.LeftButton:
+                    --segmentIndex;
+                    break;
+                case ButtonType.RightButton:
+                    break;
+                default:
+                    throw new ArgumentException(@$"{nameof(OnButtonClick)}: + Unsupported button type used.");
+            }
+
+            // Check if there is no right/left
+            if (segmentIndex < 0 || segmentIndex >= SegmentBoundaries.Count) return;
+
+            double boundaryValue = SegmentBoundaries[segmentIndex];
+
+            Value = buttonType == ButtonType.LeftButton ? boundaryValue - ViewportSize : boundaryValue;
+        }
+
+        private new double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set
+            {
+                SetValue(ValueProperty, value);
+                PreviousSegmentCommand.RaiseCanExecuteChanged();
+                NextSegmentCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (_segmentDrawing.ScrollBarCanvas != null || !(Template.FindName(@"PART_Canvas", this) is Canvas canvas)) return;
+
+            _segmentDrawing.ScrollBarCanvas = canvas;
+
+            SizeChanged += (sender, args) => _segmentDrawing.DrawSegmentBoundaries(SegmentBoundaries);
+        }
+
+        /// <summary>
+        /// Scroll segment jump behavior
+        /// </summary>
+        private void OnScroll()
+        {
+            CheckSegmentBoundaries();
+        }
+
+        /// <summary>
+        /// Check if ScrollBar thumb is at a segment boundary. Introduce jumping behaviour.
+        /// </summary>
+        private void CheckSegmentBoundaries()
+        {
+            double? boundaryValue = SegmentBoundaries.Find(s => s > Value && s < (Value + ViewportSize));
+
+            if (!(boundaryValue is {} boundary) || boundary == 0) return;
+
+            double halfThumbValue = Value + (ViewportSize / 2);
+
+            // Jump to the left or right of a segment boundary
+            Value = halfThumbValue < boundary ? boundary - ViewportSize : boundary;
+        }
+
+        private class SegmentedScrollBarSegmentDrawing
+        {
+            private readonly SegmentedScrollBar _scrollBar;
+
+            public SegmentedScrollBarSegmentDrawing(SegmentedScrollBar scrollBar)
+            {
+                _scrollBar = scrollBar;
+            }
+
+            public Canvas? ScrollBarCanvas { get; set; }
+
+            public void DrawSegmentBoundaries(IReadOnlyList<double> segmentBoundaries)
+            {
+                if (ScrollBarCanvas == null) return;
+
+                ScrollBarCanvas?.Children.Clear();
+
+                foreach (double segmentBoundary in segmentBoundaries)
+                {
+                    ScrollBarCanvas?.Children.Add(CreateBoundaryLine(CalculatePixelBoundaryPosition(segmentBoundary)));
+                }
+            }
+
+            private double CalculatePixelBoundaryPosition(double segmentBoundary)
+            {
+                double fullBar = _scrollBar.Track.Maximum + _scrollBar.Track.ViewportSize;
+
+                double relativeBoundaryPosition = segmentBoundary / fullBar;
+
+                return relativeBoundaryPosition * _scrollBar.Track.ActualWidth;
+            }
+
+            private Line CreateBoundaryLine(double pixelPosition) =>
+                new Line
+                {
+                    Stroke          = new SolidColorBrush(Colors.OrangeRed),
+                    X1              = pixelPosition,
+                    X2              = pixelPosition,
+                    Y1              = 0,
+                    Y2              = _scrollBar.Track.ActualHeight,
+                    StrokeThickness = 2,
+                };
+        }
+    }
+}
